ff3713eb66d78ea6cb4718ddd3937f9a23228d5f
shark <thetruephil@googlemail.com>
2017-03-25 14:43:59 +0100

    Installing new packages

---
+++ emacs/.emacs.d/elpa/helm-youtube-20161113.1848/helm-youtube-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-youtube" "20161113.1848" "Query YouTube and play videos in your browser" '((request "0.2.0") (helm "2.3.1") (cl-lib "0.5")) :url "https://github.com/maximus12793/helm-youtube" :keywords '("youtube" "multimedia"))
