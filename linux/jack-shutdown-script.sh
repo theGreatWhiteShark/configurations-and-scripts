52919defecbb33e4ec6009764ba4b80ca7c3fffd
theGreatWhiteShark <thetruephil@googlemail.com>
2018-07-29 17:34:43 +0200

    updating Jack configuration

--- linux/jack-shutdown-script.sh
+++ linux/jack-shutdown-script.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 ## Small bash script running whenever the JACKd sound server is shut
-## down. (Jack2 version 1.9.10)
+## down. (Jack2 version 1.9.12)
 ##
 ## On my laptop I will use the JACK system only when I record or
 ## manipulate audio. In all other instances I will rely on the
 ## PulseAudio system. Therefore, each time the JACK2d daemon is getting
-## start up, QJackCtrl will run `pasusender -- jack_control start`.
+## start up, QJackCtrl will run `pasusender -- jackd`.
 ## This will suspend PulseAudio.
 
 ## Halt the JACK audio server. Unfortunately the JACK server has to be
 ## stopped manually.
-# jack_control stop
+killall jackd
 
-## Kill the PulseAudio server
-# pulseaudio -k
-## and restart it
-# pulseaudio -D
+## Now that the JACK server is terminated. The output of PulseAudio is
+## reset to the external USB sound card.
+
+## Since the precise device names may change, just the overall label
+## of the sound card is set and the actual name will be queried.
+soundCardName="US-2x2"
 ## Set the default sink to the USB audio interface
-pacmd set-default-sink 'alsa_output.usb-TEAC_Corporation_US-2x2-00.analog-stereo'
-pacmd set-default-source 'alsa_input.usb-TEAC_Corporation_US-2x2-00.analog-stereo'
+pacmd set-default-sink $(pacmd list-sinks | grep $soundCardName | grep output | sed 's/^[[:space:]]name: <//g; s/>//g')
+pacmd set-default-source $(pacmd list-sources | grep $soundCardName | grep input | sed 's/^[[:space:]]name: <//g; s/>//g')
+
 ## Give the system time to settle down
 sleep 2
 ## Restart the PulseAudio system tray (to update the changes)
--- linux/jack-startup-script.sh
+++ linux/jack-startup-script.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+## Since I'm not using the DBus interface of JACK in Devuan ASCII, I
+## need to load the JACK sink and source first. With the DBus
+## interface active PulseAudio will load them automatically.
+pacmd load-module module-jack-sink
+pacmd load-module module-jack-source
+
 ## Setting the JACK sink as the default sink of the Pulseaudio server.
 pacmd set-default-sink 'jack_out'
 pacmd set-default-source 'jack_in'
