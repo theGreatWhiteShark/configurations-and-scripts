538e8550f7057576f6eb8838304f00e2c9698122
theGreatWhiteShark <thetruephil@googlemail.com>
2019-10-17 00:32:12 +0200

    stopping clementine and killing blocking processes

--- i3/i3suspend_and_lock.sh
+++ i3/i3suspend_and_lock.sh
@@ -17,11 +17,44 @@ if [ -d "/media/black/" ];then
 	## If the clementine player is running, it probably plays back
 	## some song from the hard disk. Thus, make it stop first.
 	if [ "$(ps -aux | grep clementine | wc -l)" -gt 1 ];then
+		## Running pause first to immediately stop the player without
+		## triggering a fadeout.
+		clementine --pause
 		clementine --stop
 	fi
 
+	## ---------------------------------------------------------------
+
 	## Actual unmounting.
-	pumount /media/black
+	##
+	## This can take a while depending on what clementine is
+	## doing. Let's give it some tries. I total it waits for 8
+	## seconds.
+	for ii in {1..10};do
+		pumount /media/black &> /dev/null
+
+		## Check whether the unmounting was successful.
+		if [ $? -eq 0 ];then
+			break
+		fi
+
+		## Was not successful yet. Let's wait and try again.
+		sleep 0.9
+
+		if [ "$ii" -eq "10" ];then
+			## Well, fuck it. Let's kill all commands accessing the
+			## hard disk.
+			for ii in $(lsof +D ~/Downloads/ -a -u$USER +c 0 | 	awk 'NR>1 {print $1}');do
+				killall $ii;
+			done
+
+			## Let's wait five more seconds for everything to settle
+			## and call pumount again. But this time without
+			## discarding its output.
+			pumount /media/black
+		fi
+
+	done
 fi
 
 ## -------------------------------------------------------------------
@@ -31,18 +64,12 @@ sudo pm-suspend
 
 ## -------------------------------------------------------------------
 
-## If the external hard disk is plugged in, mount it.
-if [ "$(ls /dev/disk/by-id/ | grep ata-TOSHIBA_MK1059GSM_11LNTERLT | wc -m)" -gt "0" ];then
-	pmount /dev/disk/by-id/ata-TOSHIBA_MK1059GSM_11LNTERLT black
-fi
-
-## -------------------------------------------------------------------
-
 ## Lock the screen
 i3lock -i /tmp/screen_shot.png
 
 ## -------------------------------------------------------------------
 
-## turn of the monitor after a short delay
-## using pgrep to NOT turn it of if unlocked in the short delay
-sleep 60; pgrep i3lock && xset dpms force off
+## If the external hard disk is plugged in, mount it.
+if [ "$(ls /dev/disk/by-id/ | grep ata-TOSHIBA_MK1059GSM_11LNTERLT | wc -m)" -gt "0" ];then
+	pmount /dev/disk/by-id/ata-TOSHIBA_MK1059GSM_11LNTERLT black
+fi
