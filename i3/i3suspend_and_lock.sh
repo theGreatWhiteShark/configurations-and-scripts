0d97afb590b5a611287738abd8e3422c2c0cbf1f
theGreatWhiteShark <thetruephil@googlemail.com>
2019-10-16 23:56:08 +0200

    using pmount and stopping clementine in suspend script

--- i3/i3suspend_and_lock.sh
+++ i3/i3suspend_and_lock.sh
@@ -11,8 +11,30 @@ mogrify -scale 20% -scale 500% /tmp/screen_shot.png
 
 ## -------------------------------------------------------------------
 
+## Unmount the external hard disk.
+if [ -d "/media/black/" ];then
+
+	## If the clementine player is running, it probably plays back
+	## some song from the hard disk. Thus, make it stop first.
+	if [ "$(ps -aux | grep clementine | wc -l)" -gt 1 ];then
+		clementine --stop
+	fi
+
+	## Actual unmounting.
+	pumount /media/black
+fi
+
+## -------------------------------------------------------------------
+
 ## Suspend the system into memory
 sudo pm-suspend
+
+## -------------------------------------------------------------------
+
+## If the external hard disk is plugged in, mount it.
+if [ "$(ls /dev/disk/by-id/ | grep ata-TOSHIBA_MK1059GSM_11LNTERLT | wc -m)" -gt "0" ];then
+	pmount /dev/disk/by-id/ata-TOSHIBA_MK1059GSM_11LNTERLT black
+fi
 
 ## -------------------------------------------------------------------
 
