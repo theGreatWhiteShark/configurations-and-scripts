a53a786dd19d4e7bf9ef17711e0365966e896a08
theGreatWhiteShark <princess.trudildis@posteo.de>
2023-09-21 11:11:08 +0200

    i3: port aspire scripts to Guix System

--- bash/backlight_aspire_s3.sh
+++ bash/backlight_aspire_s3.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 function set_aspire_s3_backlight {
 	# This script will increase (or decrease) the backlight by a
@@ -25,46 +25,46 @@ function set_aspire_s3_backlight {
 	# ----------------------------------------------------------------
 
 	if [ $1 -gt 0 ]; then
-		sudo brightnessctl -d intel_backlight s +$1%
+		brightnessctl -d intel_backlight s +$1%
 	else
-		sudo brightnessctl -d intel_backlight s $(echo "-1 * $1" |bc)%-
+		brightnessctl -d intel_backlight s $(echo "-1 * $1" |bc)%-
 	fi
 }
 
 # --------------------------------------------------------------------
 
-function set_external_monitor_brightness {
-	# This script assumes the an integer argument specifying the
-	# percentage of change. Negative values correspond to a decrease
-	# of the brightness and positive ones to an increase. -10
-	# e.g. does decrease the brightness by 10%.
-	#
-	# The second parameter is assumed to be the name of the
-	# corresponding X11 device.
+# function set_external_monitor_brightness {
+# 	# This script assumes the an integer argument specifying the
+# 	# percentage of change. Negative values correspond to a decrease
+# 	# of the brightness and positive ones to an increase. -10
+# 	# e.g. does decrease the brightness by 10%.
+# 	#
+# 	# The second parameter is assumed to be the name of the
+# 	# corresponding X11 device.
 
-	# If there is no brightness file present yet, create one.
-	if [ ! -f "$HOME/.i3/brightness" ]; then
-		echo "1" > $HOME/.i3/brightness
-	fi
-	
-	# ----------------------------------------------------------------
-	# Since this has to be done directly via X11 and xrandr, we have
-	# to use floating numbers in here. This is just a little bit a
-	# pain in the ass. Let's solve this using AWK!
-	
-	new_value=$(awk -v change="$1" '{change_relative=change/100; brightness_new=$1 + change_relative; if(brightness_new>1)print 1; else if(brightness_new < 0) print 0; else print brightness_new}' $HOME/.i3/brightness)
-	
-	# ----------------------------------------------------------------
-	# Store the new value on disk
+# 	# If there is no brightness file present yet, create one.
+# 	if [ ! -f "$HOME/.i3/brightness" ]; then
+# 		echo "1" > $HOME/.i3/brightness
+# 	fi
 
-	echo "$new_value" > $HOME/.i3/brightness
-	
-	# ----------------------------------------------------------------
-	# Set the brightness to the new value
+# 	# ----------------------------------------------------------------
+# 	# Since this has to be done directly via X11 and xrandr, we have
+# 	# to use floating numbers in here. This is just a little bit a
+# 	# pain in the ass. Let's solve this using AWK!
 
-	xrandr --output $2 --brightness $new_value
-	
-}
+# 	new_value=$(awk -v change="$1" '{change_relative=change/100; brightness_new=$1 + change_relative; if(brightness_new>1)print 1; else if(brightness_new < 0) print 0; else print brightness_new}' $HOME/.i3/brightness)
+
+# 	# ----------------------------------------------------------------
+# 	# Store the new value on disk
+
+# 	echo "$new_value" > $HOME/.i3/brightness
+
+# 	# ----------------------------------------------------------------
+# 	# Set the brightness to the new value
+
+# 	xrandr --output $2 --brightness $new_value
+
+# }
 
 # --------------------------------------------------------------------
 # Adjust the brightness of the Aspire S3
@@ -76,14 +76,14 @@ set_aspire_s3_backlight $1
 # done via a state file but only via X11 itself (at least to my
 # knowledge).
 
-# Corresponding device name.
-device_name=$(xrandr --current | sed -e 's/disconnected//g' |
-				  awk '/connected/ {if ($1 != "LVDS-1") print $1}')
+# Corresponding device name# .
+# device_name=$(xrandr --current | sed -e 's/disconnected//g' |
+# 				  awk '/connected/ {if ($1 != "LVDS-1") print $1}')
 
 # Check whether there is an external monitor connected.
-if [ $(echo $device_name | wc -m) -gt 1 ]; then
-	# There is a monitor present.
+# if [ $(echo $device_name | wc -m) -gt 1 ]; then
+# 	# There is a monitor present.
 
-	set_external_monitor_brightness $1 $device_name
+# 	set_external_monitor_brightness $1 $device_name
 
-fi
+# fi
--- i3/i3suspend_and_lock.sh
+++ i3/i3suspend_and_lock.sh
@@ -16,63 +16,10 @@ mogrify -scale 20% -scale 500% /tmp/screen_shot.png
 
 ## -------------------------------------------------------------------
 
-## Unmount the external hard disk.
-if [ -d "/media/black/music" ];then
-
-	## If the clementine player is running, it probably plays back
-	## some song from the hard disk. Thus, make it stop first.
-	if [ "$(ps -aux | grep clementine | wc -l)" -gt 1 ];then
-		## Running pause first to immediately stop the player without
-		## triggering a fadeout.
-		clementine --pause
-		clementine --stop
-	fi
-
-	## ---------------------------------------------------------------
-
-	## Actual unmounting.
-	##
-	## This can take a while depending on what clementine is
-	## doing. Let's give it some tries. I total it waits for 8
-	## seconds.
-	for ii in {1..10};do
-		pumount /media/black &> /dev/null
-
-		## Check whether the unmounting was successful.
-		if [ $? -eq 0 ];then
-			break
-		fi
-
-		## Was not successful yet. Let's wait and try again.
-		sleep 0.9
-
-		if [ "$ii" -eq "10" ];then
-			## Well, fuck it. Let's kill all commands accessing the
-			## hard disk.
-			for ii in $(lsof +D ~/Downloads/ -a -u$USER +c 0 | 	awk 'NR>1 {print $1}');do
-				killall $ii;
-			done
-
-			## Let's wait five more seconds for everything to settle
-			## and call pumount again. But this time without
-			## discarding its output.
-			pumount /media/black
-		fi
-
-	done
-fi
-
-## -------------------------------------------------------------------
-
 ## Suspend the system into memory
-sudo pm-suspend
+loginctl suspend
 
 ## -------------------------------------------------------------------
 
 ## Lock the screen
 i3lock -i /tmp/screen_shot.png
-
-## -------------------------------------------------------------------
-
-## If the external hard disk is plugged in, mount it.
-source ~/.profile
