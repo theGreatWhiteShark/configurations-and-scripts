cc523891f3cc360afce4c71731cac98bc1540f46
theGreatWhiteShark <princess.trudildis@posteo.de>
2024-07-27 10:33:27 +0200

    doom: add dts-mode

--- emacs/.doom.d/packages.el
+++ emacs/.doom.d/packages.el
@@ -10,6 +10,7 @@
 ;(package! some-package)
 (package! i3wm-config-mode)
 (package! journalctl-mode)
+(package! dts-mode)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
